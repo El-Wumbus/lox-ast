72fac3377f741fe52136f699ca8491fe3d49da5c
Aidan Neal <decator.c@proton.me>
2023-01-21 14:30:59 -0500

    Interpreter tests

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -9,7 +9,28 @@ impl ExprVisitor<Object> for Interpreter
         Ok(expr.value.to_owned().unwrap())
     }
 
-    fn visit_binary_expr(&self, expr: &BinaryExpr) -> Result<Object, LoxError> { Ok(Object::Nil) }
+    fn visit_binary_expr(&self, expr: &BinaryExpr) -> Result<Object, LoxError>
+    {
+        let left = self.evaluate(&expr.left)?;
+        let right = self.evaluate(&expr.right)?;
+
+        let res = match expr.operator.token_type()
+        {
+            TokenType::Minus => Ok(left - right),
+            TokenType::Slash => Ok(left / right),
+            TokenType::Star => Ok(left * right),
+            _ => todo!(),
+        };
+
+        if res == Ok(Object::ArithmeticError)
+        {
+            Err(LoxError::error(expr.operator.line, "Illegal expression"))
+        }
+        else
+        {
+            res
+        }
+    }
 
     fn visit_grouping_expr(&self, expr: &GroupingExpr) -> Result<Object, LoxError>
     {
@@ -30,7 +51,7 @@ impl ExprVisitor<Object> for Interpreter
                     _ => Ok(Object::Nil),
                 }
             }
-            TokenType::Bang => Ok(Object::Bool(self.is_truthy(&right))),
+            TokenType::Bang => Ok(Object::Bool(!self.is_truthy(&right))),
 
             _ => Err(LoxError::error(expr.operator.line, "Unreachable error")),
         }
@@ -45,5 +66,87 @@ impl Interpreter
     {
         // `Nil` and `False` values are false, everything else is true
         !matches!(object, Object::Nil | Object::Bool(false))
+    }
+}
+
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    use crate::tokens::*;
+
+    fn make_literal_num(n :f64) -> Box<Expr>
+    {
+        Box::new(Expr::Literal(LiteralExpr {
+            value: Some(Object::Num(n)),
+        }))
+    }
+
+    #[test]
+    /// Tests unary minus (-15) or (-value)
+    fn test_unary_minus()
+    {
+        let i = Interpreter {};
+        let unary_expr = UnaryExpr {
+            operator: Token::new(TokenType::Minus, "-".to_string(), None, 1),
+            right: make_literal_num(123.5),
+        };
+
+        let res = i.visit_unary_expr(&unary_expr).unwrap();
+
+        assert_eq!(res, Object::Num(-123.5));
+    }
+
+    #[test]
+    /// Tests unary not (!true)
+    fn test_unary_bang()
+    {
+        let i = Interpreter {};
+
+        let unary_expr = UnaryExpr {
+            operator: Token::new(TokenType::Bang, "!".to_string(), None, 1),
+            right: Box::new(Expr::Literal(LiteralExpr {
+                value: Some(Object::Bool(false)),
+            })),
+        };
+
+        let res = i.visit_unary_expr(&unary_expr).unwrap();
+
+        assert_eq!(res, Object::Bool(true));
+    }
+
+    #[test]
+    /// Tests binary subtraction (15 - 7)
+    fn test_binary_minus()
+    {
+        let i = Interpreter {};
+
+        let binary_expr = BinaryExpr {
+            left: make_literal_num(15.0),
+            operator: Token::new(TokenType::Minus, "-".to_string(), None, 0),
+            right: make_literal_num(7.0),
+        };
+
+        let res = i.visit_binary_expr(&binary_expr).unwrap();
+
+        assert_eq!(res, Object::Num(8.0));
+    }
+
+    #[test]
+    /// Test binary division (21 / 7)
+    fn test_binary_slash()
+    {
+        let i = Interpreter {};
+
+        let binary_expr = BinaryExpr {
+            left: make_literal_num(21.0),
+            operator: Token::new(TokenType::Slash, "/".to_string(), None, 0),
+            right: make_literal_num(7.0),
+        };
+
+        let res = i.visit_binary_expr(&binary_expr).unwrap();
+
+        assert_eq!(res, Object::Num(3.0));
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq, Clone)]
+use std::ops::{Sub, Div, Add, Mul};
+
+#[derive(Debug, PartialEq, Clone, PartialOrd)]
 #[allow(dead_code)]
 pub enum Object
 {
@@ -6,6 +8,86 @@ pub enum Object
     Str(String),
     Bool(bool),
     Nil,
+    ArithmeticError,
+}
+
+impl Sub for Object
+{
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self
+    {
+        match (self, other)
+        {
+            (Object::Num(left), Object::Num(right)) => Self::Num(left - right),
+
+            _ => Object::ArithmeticError,
+        }
+    }
+}
+
+impl Div for Object
+{
+    type Output = Self;
+
+    fn div(self, other: Self) -> Self
+    {
+        match (self, other)
+        {
+            (Object::Num(left), Object::Num(right)) => Self::Num(left / right),
+
+            _ => Object::ArithmeticError,
+        }
+    }
+
+}
+
+impl Mul for Object
+{
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self
+    {
+        match (self, other)
+        {
+            (Object::Num(left), Object::Num(right)) => Self::Num(left * right),
+
+            _ => Object::ArithmeticError,
+        }
+    }
+
+}
+
+impl Add for Object
+{
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self
+    {
+        match (self, other)
+        {
+            (Object::Num(left), Object::Num(right)) => Self::Num(left + right),
+            (Object::Str(left), Object::Str(right)) => Self::Str(format!("{left}{right}")),
+            _ => Object::ArithmeticError,
+        }
+    }
+}
+
+impl From<f64> for Object
+{
+    fn from(value: f64) -> Self { Object::Num(value) }
+}
+
+impl From<Object> for f64
+{
+    fn from(value: Object) -> Self
+    {
+        match value
+        {
+            Object::Num(x) => x,
+            _ => 0.0,
+        }
+    }
 }
 
 impl std::fmt::Display for Object
@@ -18,6 +100,7 @@ impl std::fmt::Display for Object
             Self::Num(x) => write!(f, "{x}"),
             Self::Bool(x) => write!(f, "{x}"),
             Self::Str(x) => write!(f, "{x}"),
+            ArithmeticError => panic!("Shouldn't be trying to print erronious Objects"),
         }
     }
 }
